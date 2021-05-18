0a366e61b4857cbe8eec59d762689fef6af4fcde
vayipalayamchand <vayipalayamchand@paypal.com>
2021-05-18 09:31:23 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Vanchinathan Ayipalayam Chandrasekaran on 5/18/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
